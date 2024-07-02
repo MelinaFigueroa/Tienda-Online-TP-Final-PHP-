264d0449fe8fa68876eee23adbc225c1a71ae810
MelinaFigueroa <melina.figueroa.89@gmail.com>
2024-07-01 23:28:03 -0300

    ultimo cambios para entrega

--- dataBase/tp_final_php.sql
+++ dataBase/tp_final_php.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-06-2024 a las 02:41:44
+-- Tiempo de generación: 02-07-2024 a las 04:26:58
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -83,20 +83,22 @@ CREATE TABLE `products` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `image_url` varchar(255) DEFAULT NULL,
   `categories` enum('Otoño','Invierno','Primavera','Verano','Outlet/Ofertas') NOT NULL DEFAULT 'Otoño',
-  `category_id` int(11) NOT NULL
+  `category_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `created_at`, `image_url`, `categories`, `category_id`) VALUES
-(2, 'Jeans colección otoño', 'Todos los talles desde el 36 al 56', 15000.00, '2024-06-20 21:07:36', '../uploads/Jeans.jpg', 'Otoño', 1),
-(3, 'Camisas colección primavera', 'Todos los talles desde el xs al xxxl', 20000.00, '2024-06-23 19:19:47', '../uploads/Camisas.jpg', 'Otoño', 3),
-(4, 'Remeras colección verano', 'Todos los talles desde el xxs al xxxl', 8000.00, '2024-06-23 19:51:25', '../uploads/Remeras.jpg', 'Otoño', 4),
-(7, 'Remeras', 'Remeras en oferta', 5500.00, '2024-06-23 23:57:08', '../uploads/ofertas-remeras.jpg', 'Otoño', 5),
-(8, 'Campera de dama', 'Campera para invierno super abrigada ', 1500000.00, '2024-06-24 00:04:09', '../uploads/campera-invierno.jpg', 'Otoño', 2),
-(9, 'Gorritos y ponchitos', 'Gorritos y ponchitos super calentitos', 5000.00, '2024-06-24 12:18:49', '../uploads/gorritos-ponchitos-invierno.jpg', 'Otoño', 2);
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `created_at`, `image_url`, `categories`, `category_id`, `user_id`) VALUES
+(2, 'Jeans colección otoño', 'Todos los talles desde el 36 al 56', 15000.00, '2024-06-20 21:07:36', '../uploads/Jeans.jpg', 'Otoño', 1, 0),
+(3, 'Camisas colección primavera', 'Todos los talles desde el xs al xxxl', 20000.00, '2024-06-23 19:19:47', '../uploads/Camisas.jpg', 'Otoño', 3, 0),
+(4, 'Remeras colección verano', 'Todos los talles desde el xxs al xxxl', 8000.00, '2024-06-23 19:51:25', '../uploads/Remeras.jpg', 'Otoño', 4, 0),
+(7, 'Remeras', 'Remeras en oferta', 5500.00, '2024-06-23 23:57:08', '../uploads/ofertas-remeras.jpg', 'Otoño', 5, 0),
+(8, 'Campera de dama', 'Campera para invierno super abrigada ', 1500000.00, '2024-06-24 00:04:09', '../uploads/campera-invierno.jpg', 'Otoño', 2, 0),
+(9, 'Gorritos y ponchitos', 'Gorritos y ponchitos super calentitos', 5000.00, '2024-06-24 12:18:49', '../uploads/gorritos-ponchitos-invierno.jpg', 'Otoño', 2, 0),
+(10, 'Lentes de sol', 'Lentes de sol, no te quedes sin el tuyo!.', 10000.00, '2024-07-01 21:49:59', '../uploads/lentes-outlet.jpg', 'Otoño', 5, 0);
 
 -- --------------------------------------------------------
 
@@ -116,16 +118,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (1, 'melina.figueroa.89@gmail.com', '$2y$10$xSUF9wLTo2wV17nDbpE1JOlyd2jqEKqHUnJQcSjqmGwk6OFR4hTYi'),
-(2, 'lalala@gmail.com', '$2y$10$EV7Rx3fQWOyFh1Fe3l/YaO2Oi2jywpowLfB4R5N/BF8t8xTUfDZeO'),
-(4, 'holamundo@gmail.com', '$2y$10$jxqPU/3WFGxO/ArXWTF5JujNd5cmMdJCMcqfLJ4OlY8BX1PDSXeVG'),
-(5, 'hola-mundo@gmail.com', '$2y$10$Bxh.UaFx1cCwl6Hd1YfUWe.AL4a/kqKQX/oFIbcUlz8cWmNTnSUHS'),
-(6, 'ejemplo@gmail.com', '$2y$10$jUxa4QKGX0dwIX9esmFw5uq.28Zugc4UUNZbT4cjXfjgkGa0KYgPO'),
-(7, 'admin@gmail.com', '$2y$10$/xcFOG/madhwjHEmG9sPDu41GYH57nAmYgSKR/E8xYmXaI4wulSya'),
-(8, 'ejemplo2@gmail.com', '$2y$10$8F9Ns1AHJQw2iY7d25xomOZRk7wfeQvVK46aKnjuwZyIwI0T.Mhqi'),
-(9, 'ejemplo3@gmail.com', '$2y$10$O8pP7Au3eejLuAX34G99Melb7DeU84ClXnn7za/L/4lr0LwxlxsDS'),
-(10, 'ejemplo4@gmail.com', '$2y$10$d6zzo6N1ksqSy6w.KwXEy.3gvzrq3CHXkGa3lgtzfk2InxjAfIMqm'),
-(11, 'holala@gmail.com', '$2y$10$D6slU71gjTPeqVkD3ZTaeeNdmtt1DuyKsEe.8sY3ezOLBHU8uIqJi'),
-(12, 'hola.mundo@gmail.com', '$2y$10$DJ5mbuzpC4QyG25pr/ZpS.Y9jAtcqlUhBpNilaq.w449OzMV.9S.6');
+(26, 'hola-mundo.89@gmail.com', '$2y$10$lbj8AodtvBFciOfYImMzieYTAR29QWDGiI27eScGkxK6uHJM..Jd6');
 
 --
 -- Índices para tablas volcadas
@@ -178,13 +171,13 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Restricciones para tablas volcadas
